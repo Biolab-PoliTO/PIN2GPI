14a36ca010fae97b999b7f9c10ecf91fefa3696e
Nicolas Leo <nicolas.leo@polito.it>
2025-02-07 16:49:13 +0100

    Add plot and comments

--- PI2GPI.m
+++ PI2GPI.m
@@ -15,7 +15,8 @@
 %            PolitoBIOMed Lab and BIOLAB, Politecnico di Torino, Turin, Italy
 %
 % File: Pi2GPI.m
-% Date: 06-02-2025 (Data export)
+% Date: 07-02-2025 (Plot and comments)
+%       06-02-2025 (Data export)
 %       05-02-2025 (Code fixing and optimization)
 %       03-02-2024 (Documentations)
 % -------------------------------------------------------------------------
@@ -57,54 +58,65 @@ disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - Loading *.mat f
 % anatomical contact points of the foot: heel, fifth metatarsal head (head5),
 % and first metatarsal head (head1).
 
-% IMPORTANT: The number and composition of the spatial clusters should be 
-% adjusted to best fit the specific requirements of your study.
-disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - Set spatial clusters...']);
-cluster_name = {'heel', 'head5', 'head1'}; % Set clusters' names
-cluster_channels = {          % Assignment of channels to each cluster
-    [12, 13, 14, 15, 16], ... % Cluster 1: heel
-    [5, 9, 10, 11], ...       % Cluster 2: 5th Metatarsal Head
-    [1, 2, 3, 4, 6, 7, 8]};   % Cluster 3: 1st Metatarsal Head
-
-% Set foot sensing resistors' coordinates
-x = [1, 1, 2, 3, 4, 1, 2, 3, 4, 4, 4, 4, 3, 4, 2, 3];
-y = [13, 11.5, 11.5, 11.5, 10.5, 10, 10, 10, 9, 7.5, 6, 3.5, 2, 2, 0.5, 0.5];
-
-% Plot cluster configuration
-% --------------------------
-figure; hold on;
-scatter(x, y, 100, 'k', 'filled');
-
-colors = {[0, 114, 189] / 255,  [119, 172, 48] / 255, [162, 20, 47] / 255};
-labels = {'Heel', '5th Metatarsal head', '1st Metatarsal head'};
-
-for i = 1:3 % Loop over clusters
-    scatter(x(cluster_channels{i}), y(cluster_channels{i}), 300, colors{i}, ...
-        'filled', 'DisplayName', labels{i});
-end
-
-text(x-0.2, y, cellstr(num2str((1:length(x))')), 'FontSize', 10, 'FontWeight', ...
-    'bold', 'Color', 'w');
-
-xlim([-6 10]); ylim([-1 16]);
-legend([{''}, labels(:)'], 'Location', 'Best');
-title('Current cluster configuration'); axis off; grid off;
-disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - Set spatial clusters...Ok']);
-hold off;
-
-clear colors labels x y
-
-%% Pre-processing and detection of activation windows for each cluster
-% --------------------------------------------------------------------
-% Setting parameters
-% ------------------
-minPeakHeight = 0.01; % The lowest amplitude a signal must reach to be 
-                      % considered as 'active', filtering out low-amplitude 
-                      % fluctuations likely caused by noise.
-sides = fieldnames(data); % sides: 'LeftFoot' and 'RightFoot'
+sides = fieldnames(data); % sides: LeftFoot and RightFoot
 
 for s = 1:length(sides) % Loop over sides
+    % IMPORTANT: The number and composition of the spatial clusters should be 
+    % adjusted to best fit the specific requirements of your study.
+    disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
+        char(sides(s)) ' - Set spatial clusters...']);
+    cluster_name = {'heel', 'head5', 'head1'}; % Set clusters' names
+    cluster_channels = {          % Assignment of channels to each cluster
+        [12, 13, 14, 15, 16], ... % Cluster 1: heel
+        [5, 9, 10, 11], ...       % Cluster 2: 5th Metatarsal Head
+        [1, 2, 3, 4, 6, 7, 8]};   % Cluster 3: 1st Metatarsal Head
     
+    % Set foot sensing resistors' coordinates
+    x = [1, 1, 2, 3, 4, 1, 2, 3, 4, 4, 4, 4, 3, 4, 2, 3];
+    y = [13, 11.5, 11.5, 11.5, 10.5, 10, 10, 10, 9, 7.5, 6, 3.5, 2, 2, 0.5, 0.5];
+    
+    % Plot cluster configuration
+    % --------------------------
+    figure('Name', sprintf('%s Results Plot', sides{s}), 'NumberTitle', 'off', ...
+        'Units','centimeters','Position',[5,5,40,20]);
+    subplot(2,2,[1 3]), hold on;
+    scatter(x, y, 100, 'k', 'filled');
+    
+    colors = {[0, 114, 189] / 255,  [119, 172, 48] / 255, [162, 20, 47] / 255};
+    labels = {'Heel', '5th Metatarsal head', '1st Metatarsal head'};
+    
+    for i = 1:3 % Loop over clusters
+        scatter(x(cluster_channels{i}), y(cluster_channels{i}), 300, colors{i}, ...
+            'filled', 'DisplayName', labels{i});
+    end
+    
+    % Modify based on the side (left or right)
+    % ---------------------------------------
+    if strcmp(sides{s}, 'LeftFoot')  % If the side is left
+        text(x+0.2, y, cellstr(num2str((1:length(x))')), 'FontSize', 10, 'FontWeight', 'bold', 'Color', 'w');
+        set(gca, 'XDir', 'reverse');  % Reverse the X axis for the left side
+    else
+        text(x-0.2, y, cellstr(num2str((1:length(x))')), 'FontSize', 10, 'FontWeight', 'bold', 'Color', 'w');
+    end
+    
+    xlim([-6 10]); ylim([-1 16]);
+    legend([{''}, labels(:)'], 'Location', 'Best');
+    title(sprintf('%s current cluster configuration', sides{s})); grid off;
+    disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
+        char(sides(s)) ' - Set spatial clusters...Ok']);
+    hold off;
+    set(gca, 'XColor', 'w', 'YColor', 'w')
+    
+    clear labels x y
+    
+    %% Pre-processing and detection of activation windows for each cluster
+    % --------------------------------------------------------------------
+    % Setting parameters
+    % ------------------
+    minPeakHeight = 0.01; % The lowest amplitude a signal must reach to be 
+                          % considered as 'active', filtering out low-amplitude 
+                          % fluctuations likely caused by noise.
+
     disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
         char(sides(s)) ' - Pre-processing and detection of activation windows...']);
 
@@ -113,21 +125,27 @@ for s = 1:length(sides) % Loop over sides
     
         % Pre-processing: combine, normalize, and smooth signals
         % ------------------------------------------------------
-        clus_signals_sum = mean(sum(data.(sides{s})(:, cluster_channels{clus}), 2), 2); % Combine and normalize signals
-        clus_signals_der = smooth([0; diff(smooth(clus_signals_sum, 11))]); % Smooth and compute first derivative
+        clus_signals_sum.(cluster_name{clus}) = mean(data.(sides{s})(:, ...
+            cluster_channels{clus}), 2); % Combine and normalize signals
+        clus_signals_der.(cluster_name{clus}) = smooth([0; diff(smooth(clus_signals_sum.(cluster_name{clus})...
+            , 11))]); % Smooth and compute first derivative
 
         % Detection of maximum and minimum peaks
         % --------------------------------------
         [maxPeaks.(cluster_name{clus}), maxLocs.(cluster_name{clus})] = ...
-            findpeaks(clus_signals_der, "MinPeakHeight", minPeakHeight);
+            findpeaks(clus_signals_der.(cluster_name{clus}), "MinPeakHeight", minPeakHeight);
         [minPeaks.(cluster_name{clus}), minLocs.(cluster_name{clus})] = ...
-            findpeaks(-clus_signals_der, "MinPeakHeight", minPeakHeight);
+            findpeaks(-clus_signals_der.(cluster_name{clus}), "MinPeakHeight", minPeakHeight);
         minPeaks.(cluster_name{clus}) = -minPeaks.(cluster_name{clus}); % Invert minima
 
-        % Activation window detection
+        % Activation Window (AW) detection
         % ---------------------------
-        % BRIEFLY DESCRIBE THE FUNCTIONING OF THE ALGORITHM!
-        
+        % AW is defined as the time interval between the current maximum and
+        % the lowest minimum before the subsequent maximum. In cases where 
+        % two consecutive maxima occur without an intermediate minimum, the 
+        % maximum with the highest amplitude is selected as the activation 
+        % onset and the next occurring minimum is used as the activation 
+        % offset. 
         activation.(cluster_name{clus}) = zeros(1, num_samples);
 
         for i = 1:length(maxLocs.(cluster_name{clus}))-1 % Loop over maximum peaks
@@ -172,13 +190,8 @@ for s = 1:length(sides) % Loop over sides
     
     disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
         char(sides(s)) ' - Gait cycle subphase identification...']);
-
-    % Initialize variables
-    % --------------------
-    phase_string = repmat(' ', 1, num_samples);
-    phase_num = zeros(1, num_samples);
     
-    % Identify gait subphase according to activation rules
+     % Identify gait subphase according to activation rules
     % ----------------------------------------------------
     output.(sides{s}) = ones(1, num_samples) * 4; % Default phase is swing (output = 4)
     output.(sides{s})(activation.heel & ~(activation.head1 | activation.head5)) = 1; % Heel phase (output = 1)
@@ -189,10 +202,35 @@ for s = 1:length(sides) % Loop over sides
         char(sides(s)) ' - Gait cycle subphase identification...Ok']);
 
     % Represent gait subphases
-    % ------------------------
-    % INSERT HERE BASO SIGNALS!
+    % ------------------
+    disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
+    char(sides(s))    ' - Results visualization...']);
 
+    % Plot results
+    % -----------------
+    subplot(2,2,2), hold on;
+    for clus = 1:length(cluster_channels) % Loop over clusters
+         plot(data.(sides{s})(:, cluster_channels{clus}),  'Color', colors{clus}), hold on,
+    end
+    title('PI signals')
+    ylabel('Pressure Amplitude (a.u.)'),
+    ylim([-0.1 1.1]),yticks([0 1]), set(gca, 'XTickLabel', []);
+   
+    subplot(2,2,4)
+    stairs(output.(sides{s}),'Color','k', 'LineWidth', 2),
+    yticks([0 1 2 3 4 5]), yticklabels({'','H', 'F', 'P', 'S', ''});
+    xlabel('Time (sample)'), ylim([0 5]);
+    title(sprintf('%s Basographic Signal', sides{s}))
+    hold off, box off, 
+    
+    axesHandles = findall(gcf, 'Type', 'axes');
+    axesToLink = [axesHandles(2), axesHandles(1)]; 
+    linkaxes(axesToLink, 'x');
+    xlim([-0.1 num_samples]);
+    disp([char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss')) ' - ' ...
+    char(sides(s)) ' - Results visualization...Ok']);
 end
+
 
 %% Export data
 % ------------
